c298dbd11e9e615859cb4f2596772f724ecba756
efcampoy <eliseofernandezCampoy@gmail.com>
2024-11-23 13:52:24 +0100

    bucket s3 terminado

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,8 @@ output "building_public_ip" {
   description = "IP pública de la instancia EC2"
   value       = aws_instance.building_web.public_ip
 }
+
+output "s3_bucket_url" {
+  description = "URL del bucket para acceso directo"
+  value       = aws_s3_bucket.my_bucket_building
+}
